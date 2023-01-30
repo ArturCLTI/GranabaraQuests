c2d3467370acde4a649cbf9720d2cb34997af7eb
Artur da Costa <arturclti@outlook.com>
2023-01-30 17:11:41 -0300

    Exercicios de JavaScript do Curso do Guanabara

--- Exercicio 1/src/script.js
+++ Exercicio 1/src/script.js
@@ -1,28 +1,26 @@
 function carregar(){
+  const data = new Date();
 
-  const data = new Date()
+  const msg = document.getElementById("msg");
 
-  const msg = document.getElementById('msg');
+  const img = document.getElementById("imagem");
 
-  const img = document.getElementById('imagem');
+  // const hora = data.getHours();
+  let hora = 24;
 
- // const hora = data.getHours()
-  const hora = 20
-
-  msg.innerHTML = `Agora são ${hora} horas.`
-  console.log (msg)
-   if ( hora >=0 && hora <12) {
+  msg.innerHTML = `Agora são ${hora} horas.`;
+  console.log(msg);
+  if (hora >= 0 && hora < 12) {
     //BOM dia!
-    img.src =   '/assets/img/manha.jpg' 
-    document.body.style.background = '#E3E0CE'
-  } else if ( hora >= 12 && hora < 18) {
-    //bom tarde
-    img.src = '/assets/img/tarde.jpg' 
-    document.body.style.background = '#B67749'
+    img.src = "/Exercicio 1/assets/img/manha.jpg";
+    document.body.style.background = "#E3E0CE";
+  } else if (hora >= 12 && hora < 18) {
+    //boa tarde
+    img.src = "/Exercicio 1/assets/img/tarde.jpg";
+    document.body.style.background = "#B67749";
   } else {
     //boa noite
-    img.src =  '/assets/img/noite.jpg' 
-    document.body.style.background = '#1A1F1B'
-  }  
-
+    img.src = "/Exercicio 1/assets/img/noite.jpg";
+    document.body.style.background = "#1A1F1B";
+  }
 }
